2dcda810e6d2c5742b7e0cc5ee832642c0f8fbb0
navarro <caionavarro220@gmail.com>
2025-11-03 22:03:57 -0300

    correcoes

--- src/test/java/com/example/applications/ProductApplicationTest.java
+++ src/test/java/com/example/applications/ProductApplicationTest.java
@@ -4,46 +4,53 @@ import com.example.entities.Product;
 import com.example.services.ProductService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.NoSuchElementException;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 
-public class ProductServiceTest {
+@ExtendWith(MockitoExtension.class)
+public class ProductApplicationTest {
 
     @InjectMocks
+    private ProductApplication produtoApplication;
+
+    @Mock
     private ProductService produtoService;
 
     private Product produto;
 
     @BeforeEach
     public void setUp() {
-        produtoService = new ProductService();
+        produtoApplication = new ProductApplication(produtoService);
 
         produto = new Product(1, "Produto", 10, "C:\\Users\\Admin\\Documents\\foto.jpg");
     }
 
     @Test
     public void deveSalvarImagemCorretamente(){
-        boolean resultado = produtoService.save(produto);
+        produtoApplication.append(produto);
 
-        assertTrue(resultado);
         assertEquals(produto.getImage(), "C:\\Users\\Admin\\Documents\\foto.jpg");
     }
 
     @Test
     public void deveRemoverImagemCorretamente(){
-        produtoService.save(produto);
-        produtoService.remove(produto.getId());
+        produtoApplication.append(produto);
+        produtoApplication.remove(produto.getId());
 
-        assertThrows(NoSuchElementException.class, () -> {produtoService.getImagePathById(produto.getId());
-        } );
+        verify(produtoService, times(1)).remove(produto.getId());
     }
 
     @Test
     public void deveAtualizarImagemCorretamente(){
-        produtoService.save(produto);
+        produtoApplication.append(produto);
         assertEquals(produto.getImage(), "C:\\Users\\Admin\\Documents\\foto.jpg");
 
         produto.setImage("C:\\Users\\Admin\\Documents\\foto2.jpg");
